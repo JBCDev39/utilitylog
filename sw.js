7a8f9f7eafcafcf79c3bde33269f450d7f87f169
JBC39 <79612144+JBCDev39@users.noreply.github.com>
2026-05-04 20:03:00 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,35 +1,7 @@
-var CACHE='utilitylog-v7';
-var ASSETS=[
-  '/',
-  '/index.html',
-  '/app.js',
-  '/manifest.json',
-  '/icons/icon-192.png',
-  '/icons/icon-512.png'
-];
-
-self.addEventListener('install',function(e){
-  e.waitUntil(
-    caches.open(CACHE).then(function(cache){
-      return cache.addAll(ASSETS);
-    })
-  );
-  self.skipWaiting();
-});
-
-self.addEventListener('activate',function(e){
-  e.waitUntil(
-    caches.keys().then(function(keys){
-      return Promise.all(keys.filter(function(k){return k!==CACHE;}).map(function(k){return caches.delete(k);}));
-    })
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch',function(e){
-  e.respondWith(
-    caches.match(e.request).then(function(cached){
-      return cached||fetch(e.request).catch(function(){return cached;});
-    })
-  );
+self.addEventListener('install', function() { self.skipWaiting(); });
+self.addEventListener('activate', function(e) {
+  e.waitUntil(self.clients.claim());
+  caches.keys().then(function(keys) {
+    return Promise.all(keys.map(function(k) { return caches.delete(k); }));
+  });
 });
